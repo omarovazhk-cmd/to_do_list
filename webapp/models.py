a2d15cf8067db1b6eefc647d571eb7791451990a
Zhanar <omarovazhk@gmail.com>
2025-10-26 22:39:36 +0500

    Добавила модель Task

--- webapp/models.py
+++ webapp/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
 class Task(models.Model):
-    STATUS_CHOICES = [
-        ('new', 'Новая'),
-        ('in_progress', 'В процессе'),
-        ('done', 'Сделано'),
-    ]
+    status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
 
     description = models.TextField(verbose_name='Описание')
-    status = models.CharField(verbose_name='Статус', max_length=20, choices=STATUS_CHOICES, default='new')
+    status = models.CharField(verbose_name='Статус', max_length=20, choices=status_choices, default='new')
     due_date = models.DateField(verbose_name='Дата выполнения', blank=True, null=True)
 
 
