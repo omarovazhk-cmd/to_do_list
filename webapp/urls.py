23d58969ae59993314ec3863e3582a422932802e
Zhanar <omarovazhk@gmail.com>
2025-10-29 18:06:29 +0500

    Добавила маршрут для "подробного описания"

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from webapp.views import task_list_view, task_create_view, task_delete_view
+from webapp.views import task_list_view, task_create_view, task_delete_view, task_detail_view
 
 urlpatterns = [
     path('', task_list_view, name='task_list'),
     path('create/', task_create_view, name='task_create'),
     path('delete/<int:pk>/', task_delete_view, name='task_delete'),
+    path('task/<int:pk>/', task_detail_view, name='task_detail'),
 ]
 
 
