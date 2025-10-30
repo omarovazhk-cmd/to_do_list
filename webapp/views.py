50f711dcc158396275137a800b4e53dd326fa576
Zhanar <omarovazhk@gmail.com>
2025-10-30 15:59:29 +0500

    Добавила представление детальной страницы задач

--- webapp/views.py
+++ webapp/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from .models import Task
-from .forms import TaskForm
+from webapp.models import Task
+from webapp.forms import TaskForm
+
 
 def task_list_view(request):
-    tasks = Task.objects.all()
-    form = TaskForm(request.POST or None)
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect(reverse('task_list.html'))
-    return render(request, 'task_list.html', {'tasks': tasks, 'form': form})
+    tasks = Task.objects.order_by('status', 'due_date', 'id')
+    return render(request, 'task_list.html', {'tasks': tasks})
+
 
 def task_create_view(request):
     if request.method == 'POST':
@@ -17,18 +15,18 @@ def task_create_view(request):
         if form.is_valid():
             form.save()
             return redirect(reverse('task_list'))
-        else:
-            form = TaskForm()
-        return render(request, 'task_create.html', {'form': form})
+    else:
+        form = TaskForm()
+    return render(request, 'task_create.html', {'form': form})
+
+
+def task_detail_view(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, 'task_detail.html', {'task': task})
+
 
 def task_delete_view(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
         task.delete()
-    return redirect('task_list')
-
-def task_detail_view(request, pk):
-    task = get_object_or_404(Task, pk=pk)
     return redirect(reverse('task_list'))
-
-
