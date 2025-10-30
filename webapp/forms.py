0f139401cd1172aff6e3cda5b8401a39b80eb4a8
Zhanar <omarovazhk@gmail.com>
2025-10-30 16:02:56 +0500

    Добавила в TaskForm новое поле description_detail и применила Bootstrap-оформление для всех полей

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from webapp.models import Task
 
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ('description', 'status', 'due_date')
+        fields = ['description', 'status', 'due_date', 'description_detail']
         widgets = {
-            'description': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'rows': 2, 'class': 'form-control'}),
             'status': forms.Select(attrs={'class': 'form-select'}),
             'due_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'description_detail': forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}),
         }
