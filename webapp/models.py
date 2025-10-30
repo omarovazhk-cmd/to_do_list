807c797b0a80b8755fde2bbd75f7b679d8083e89
Zhanar <omarovazhk@gmail.com>
2025-10-30 15:20:16 +0500

    Добавила поле description_detail в модель Task

--- webapp/models.py
+++ webapp/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
+
 class Task(models.Model):
-    status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+    status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
 
     description = models.TextField(verbose_name='Описание')
     status = models.CharField(verbose_name='Статус', max_length=20, choices=status_choices, default='new')
     due_date = models.DateField(verbose_name='Дата выполнения', blank=True, null=True)
     description_detail = models.TextField('Подробное описание', blank=True, null=True)
-
-
 
     def __str__(self):
         return f'{self.id} - {self.description}'
